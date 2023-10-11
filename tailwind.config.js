0e60ed8bf97c9b54bc012441e05a876f57b69523
sujal lama <lsujal41@gmail.com>
2023-10-11 15:15:48 +0545

    page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['Roboto', 'sans-serif'],
+      display: ['Lato', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        primary: {
+          light: '#1455E1',
+          DEFAULT: '#0F40A9',
+          dark: '#071E51'
+        },
+        secondary: {
+          DEFAULT: '#EF105D'
+        }
+      },
+      container: false,
+    },
   },
   plugins: [],
 }
